8d409fc628938e0ef742a704ea5b5ef6b5262811
CkayDark <ckayhome@web.de>
2026-07-09 19:52:43 +0200

    setter für index von virtuell und phsyical

--- tlbentry.cpp
+++ tlbentry.cpp
@@ -1,5 +1,8 @@
 #include "tlbentry.h"
 
+TLBEntry::TLBEntry() {}
+
+
 unsigned char TLBEntry::getFrame_attributes() const
 {
     return frame_attributes;
@@ -15,4 +18,16 @@ unsigned int TLBEntry::getPage_index() const
     return page_index;
 }
 
-TLBEntry::TLBEntry() {}
+void TLBEntry::setFrame_attributes(unsigned char frame_attributes)  {
+    this->frame_attributes = frame_attributes;
+}
+
+void TLBEntry::setPage_frame_index(unsigned int pf_index) {
+    this->page_frame_index = pf_index;
+}
+
+void TLBEntry::setPage_index(unsigned int p_index) {
+    this->page_index = p_index;
+}
+
+
--- tlbentry.h
+++ tlbentry.h
@@ -13,6 +13,9 @@ public:
     unsigned char getFrame_attributes() const;
     unsigned int getPage_frame_index() const;
     unsigned int getPage_index() const;
+    void setFrame_attributes(unsigned char frame_attributes);
+    void setPage_frame_index(unsigned int pf_index);
+    void setPage_index(unsigned int p_index);
 };
 
 #endif // TLBENTRY_H
